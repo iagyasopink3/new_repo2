b03697c3c45987a42660f7f0788017cb04dbcbdc
NUCAL <nusec_stage_iagyasopink7@yopmail.com>
2022-10-27 15:09:05 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

--- src/components/pages/Screen1.vue
+++ src/components/pages/Screen1.vue
@@ -1,7 +1,7 @@
 <template>
   <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
     v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
-    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="1587"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="1048"
     v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
     <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
       v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
@@ -10,17 +10,17 @@
       v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
       slot="r0-c0" area="r0-c0">
       <NucGroupElement top="13.285024154589372%" left="0%" v-bind:snapX="false" v-bind:snapY="false"
-        v-bind:isLock="true" bgColor="" position="absolute" ncTooltipText="" id="71e946ec-30ec-4d75-ae6e-b37703878f28"
+        v-bind:isLock="false" bgColor="" position="absolute" ncTooltipText="" id="71e946ec-30ec-4d75-ae6e-b37703878f28"
         v-bind:style="{'color':'#FFFFFF','width':'1048.5px','height':'474px','zIndex':2,'fontSize':'11.25px','maxWidth':'','minWidth':'7.5','tabIndex':'-0.75','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'7.5','textAlign':'center','background':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.9375','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.75px','textDecoration':'none','borderLeftWidth':'0.75px','borderRightWidth':'0.75px','borderBottomWidth':'0.75px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}">
-        <WImage top="4.746835443037975%" left="0.1430615164520744%" v-bind:blank="false" v-bind:snapX="false"
-          v-bind:snapY="false" v-bind:rounded="false" stretch="none"
-          imageUrl="https://www.happierhuman.com/wp-content/uploads/2022/05/leadership-affirmations-lead-your-team.jpg"
-          position="absolute" v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black"
-          v-bind:aspectRatio="true" dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
+        <WImage top="0%" left="0.09555661729574773%" v-bind:blank="false" v-bind:snapX="false" v-bind:snapY="false"
+          v-bind:rounded="false" stretch="none"
+          imageUrl="https://i.ibb.co/Rvzn6DJ/Whats-App-Image-2022-10-27-at-15-06-28.jpg" position="absolute"
+          v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black" v-bind:aspectRatio="true"
+          dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
           v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
           ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
           id="a05b7047-d29d-4625-913d-fe7330fb975c"
-          v-bind:style="{'width':'1587px','height':'429px','zIndex':4,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+          v-bind:style="{'width':'1040px','height':'1600px','zIndex':4,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
         <WLabel top="69.4620253164557%" left="49.07010014306152%"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in orci magna. Donec commodo, eros eget hendrerit bibendum, diam tellus porttitor nisi, ut tristique orci diam sed neque. Nulla ornare nulla id odio faucibus, id tempor nulla vehicula. Aliquam ut interdum odio. Duis congue eleifend orci quis volutpat. Nam finibus condimentum turpis, pretium tempus felis consectetur non"
           v-bind:snapX="false" v-bind:snapY="true" theme="theme--light01" position="absolute" ncTypography="custom"
